1a731b837ab0aed8ed75bd32a8ec020305750183
arun2352000 <poorvaja10@gmail.com>
2024-09-25 15:49:18 +0530

    multer creation and installation

--- Controllers/product.controllers.js
+++ Controllers/product.controllers.js
@@ -2,8 +2,8 @@ import PRODUCT from "../Models/product.schema.js";
 
 export const createProduct = async(req,res) => {
     try {
-        const {name,price,description} = req.body;
-        const newProduct = new PRODUCT({name,price,description});
+        const {name,price,description,ratings,category,seller,stock,numOfReviews} = req.body;
+        const newProduct = new PRODUCT({name,price,description,ratings,category,seller,stock,numOfReviews});
         await newProduct.save()
         res.status(200).json({data: newProduct});
         } catch (error) {
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import dotenv from "dotenv"
 import cors from "cors"
+import multer from 'multer'
+
 import { connect } from "mongoose"
 import connectDB from './Database/dbConfig.js'
 import productRoutes from './Routers/product.route.js'
@@ -12,6 +14,29 @@ const app = express();
 
 app.use(cors())
 app.use(express.json());
+
+// multer start
+
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/images')
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() *1)
+        cb(null, file.originalname + '-' + uniqueSuffix + path.extname(file.originalname)  )
+    }
+})
+let maxSize = 2 * 1024 * 1024;
+let upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: maxSize
+    }
+})
+
+let uploadHandler = upload.single('file');
+
+//end of multer
 
 connectDB();
 
