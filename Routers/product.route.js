d819980bc05b5393939a290980645d9447f6a84d
arun2352000 <poorvaja10@gmail.com>
2024-10-18 09:45:34 +0530

    error configuration building basic

--- Routers/product.route.js
+++
@@ -1,7 +0,0 @@
-import { createProduct, getAllProduct } from "../Controllers/product.controllers.js"
-import express from 'express'
-
-const router=express.Router()
-router.post('/createproduct', createProduct)
-router.get('/getallproduct', getAllProduct)
-export default router;
--- index.js
+++ index.js
@@ -1,50 +1,13 @@
-import express from 'express'
-import dotenv from "dotenv"
-import cors from "cors"
-// import multer from 'multer'
+import express from "express"
+import cors from 'cors'
+import dotenvv from 'dotenv'
 
-// import { connect } from "mongoose"
-import connectDB from './Database/dbConfig.js'
-import productRoutes from './Routers/product.route.js'
 
-dotenv.config()
+dotenvv.config();
 
-const PORT = process.env.PORT
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(cors())
+app.use(cors)
 app.use(express.json());
 
-// multer start
-
-// let storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, './public/images')
-//     },
-//     filename: (req, file, cb) => {
-//         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() *1)
-//         cb(null, file.originalname + '-' + uniqueSuffix + path.extname(file.originalname)  )
-//     }
-// })
-// let maxSize = 2 * 1024 * 1024;
-// let upload = multer({
-//     storage: storage,
-//     limits: {
-//         fileSize: maxSize
-//     }
-// })
-
-// let uploadHandler = upload.single('file');
-
-// app.post ()
-//end of multer
-
-connectDB();
-
-// app.get('/')
-
-app.use('/api/product',productRoutes)
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-    })
