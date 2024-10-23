609ee69de3d375b504f4efef0ed7c50f8a4a7a93
arun2352000 <poorvaja10@gmail.com>
2024-10-23 14:09:40 +0530

    ,

--- Models/user.schema.js
+++ Models/user.schema.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema(
         token:{
             type: String,
             default: ''
-        }
+        },
         // created_at: {
         //     type: Date,
         //     default: Date.now
